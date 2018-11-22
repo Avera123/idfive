1826e9d41c85e09d4723c1763f809f09d1beaaee
Alejandro Vera Carrasquilla <avera02@misena.edu.co>
2018-11-21 22:04:37 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'barcoding');
+define('DB_NAME', 'u605702254_five');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'u605702254_five');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Avera123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
